62dcb26d7de9cb3e19884e53d111db3dd3bc0caf
hamgooof <hamgooof@googlemail.com>
2014-08-05 17:18:02 +0100

    Improved the efficiency of action bar dropping

--- src/com/fallboo/miner/tasks/OreDrop.java
+++ src/com/fallboo/miner/tasks/OreDrop.java
@@ -3,6 +3,7 @@ package com.fallboo.miner.tasks;
 import com.fallboo.miner.data.MiningStyle;
 import com.fallboo.miner.data.Ores;
 import java.util.ArrayList;
+import java.util.concurrent.Callable;
 import org.powerbot.script.Condition;
 import org.powerbot.script.Random;
 import org.powerbot.script.rt6.ClientContext;
@@ -47,19 +48,30 @@ public class OreDrop extends GraphScript.Action<ClientContext> {
         if (!ctx.combatBar.select().id(ore.getId()).isEmpty()) {
             int ores = 0;
             for (Item i : ctx.backpack.items()) {
-                if (!picks.contains(i.id())) {
+                System.out.println("Found: " + i.id());
+                if (i.id() == ore.getId()) {
                     ores++;
                 }
             }
             int key = ctx.combatBar.poll().slot() + 1;
-            for (int i = 0; i <= ores + (miningStyle == MiningStyle.M1D1 ? 0 : Random.nextInt(0, 3)); i++) {
+            for (int i = 1; i <= ores + (miningStyle == MiningStyle.M1D1 ? 0 : Random.nextInt(0, 3)); i++) {
                 ctx.input.send("{VK_" + key + "}");
                 Condition.sleep(100);
             }
         }
-        for (Item i : ctx.backpack.items()) {
-            if (!picks.contains(i.id())) {
-                i.interact("Drop");
+        if (!Condition.wait(new Callable<Boolean>() {
+
+            @Override
+            public Boolean call() throws Exception {
+                return ctx.backpack.select().isEmpty();
+            }
+        }, 250, 8)) {
+            if (!ctx.backpack.select().isEmpty()) {
+                for (Item i : ctx.backpack.items()) {
+                    if (i.valid() && !picks.contains(i.id())) {
+                        i.interact("Drop");
+                    }
+                }
             }
         }
     }
