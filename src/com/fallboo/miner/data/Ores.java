3665b8a48c2664003753f7fb72683150e5d5d9c7
hamgooof <hamgooof@googlemail.com>
2014-08-05 16:30:51 +0100

    Added Gold to Ores as well as linked Ores to Rocks so the items mining can be calculated easier

--- src/com/fallboo/miner/data/Ores.java
+++ src/com/fallboo/miner/data/Ores.java
@@ -11,7 +11,7 @@ package com.fallboo.miner.data;
  */
 public enum Ores {
 
-    COPPER(436), TIN(438), IRON(440), SILVER(442), COAL(453), MITHRIL(447), ADDY(449);
+    COPPER(436), TIN(438), IRON(440), SILVER(442), COAL(453), MITHRIL(447), ADDY(449), GOLD(358);
     private int id;
 
     Ores(int id) {
